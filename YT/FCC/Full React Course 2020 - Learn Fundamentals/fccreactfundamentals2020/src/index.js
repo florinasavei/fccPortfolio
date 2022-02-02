8a90cf9036844e9acdd417e217b8ff0efb010bbb
florin_asavei <florin.asavei@delltea.com>
2022-02-02 23:31:33 +0200

    yet anotether hello world in react

---
+++ YT/FCC/Full React Course 2020 - Learn Fundamentals/fccreactfundamentals2020/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Greeting() {
+  return <h1>Helo World</h1>
+}
+
+ReactDOM.render(<Greeting/>, document.getElementById('root'));
